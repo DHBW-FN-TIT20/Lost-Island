fe2af86f402d4bfed0b1c7d1e720b879c5a6a836
Florian Herkommer <florian.herkommer@gmx.de>
2022-11-20 16:53:17 +0000

    extendes resizer

--- src/World/systems/Resizer.js
+++ src/World/systems/Resizer.js
@@ -8,14 +8,18 @@ class Resizer {
      * @param {THREE.Renderer} renderer 
      */
     constructor(container, camera, renderer) {
+        this.setSize(container, camera, renderer);
+        window.addEventListener("resize", () => {
+            // set the size again if a resize occurs
+            this.setSize(container, camera, renderer);
+        });
+    }
+    setSize(container, camera, renderer) {
         // Set the camera's aspect ratio
         camera.aspect = container.clientWidth / container.clientHeight;
-
         camera.updateProjectionMatrix();
-
         // update the size of the renderer AND the canvas
         renderer.setSize(container.clientWidth, container.clientHeight);
-
         // set the pixel ratio (for mobile devices)
         renderer.setPixelRatio(window.devicePixelRatio);
     }
