78323782d56e368082ecc50afc6496b15ccebe78
Floskinner <58706771+Floskinner@users.noreply.github.com>
2022-11-20 16:19:08 +0100

    added controll help text

--- src/main.js
+++ src/main.js
@@ -1,5 +1,14 @@
 import { World } from './World/World.js';
 
+const controllHelpDiv = document.getElementById("controllHelp");
+const controllShowDiv = document.getElementById("controllShow");
+
+document.body.addEventListener("keydown", (e) => {
+    if (e.key === "h" || e.key === "H") {
+        controllHelpDiv.style.display = controllHelpDiv.style.display === "none" ? "block" : "none";
+        controllShowDiv.style.display = controllShowDiv.style.display === "none" ? "block" : "none";
+    }
+});
 
 async function main() {
     const spinner = document.getElementById("spinner");
